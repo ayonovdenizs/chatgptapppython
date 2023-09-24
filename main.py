1201dcdd822cbfb86bcf8581031937f83bdf0101
Denis <77270222+ayonovdenizs@users.noreply.github.com>
2023-09-24 10:26:42 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,37 +1,27 @@
-import kivy
-import g4f
-from kivy.app import App
-from kivy.uix.label import Label
-from kivy.uix.button import Button
-from kivy.uix.boxlayout import BoxLayout
-from kivymd.uix.textfield import MDTextField
+from kivy.lang import Builder
 
-class MyApp(App):
+from kivymd.app import MDApp
+
+
+KV = """
+Screen:
+
+    MDToolbar:
+        title: "My firt app"
+        elevation: 10
+        md_bg_color: app.theme_cls.primary_color
+        left_action_items: [["menu", lambda x: x]]
+        pos_hint: {"top": 1}
+
+    MDRaisedButton:
+        text: "Hello World"
+        pos_hint: {"center_x": .5, "center_y": .5}
+"""
+
+
+class HelloWorld(MDApp):
     def build(self):
-        # Создаем виджет Label
-        self.label = Label(text='Добро пожаловать в ChatGPTapp!')
+        return Builder.load_string(KV)
 
-        # Создаем виджет MDTextField
-        self.text_input = MDTextField(hint_text='Введи запрос')
 
-        # Создаем кнопку "Отправить"
-        self.button = Button(text='Отправить')
-        self.button.bind(on_press=self.on_button_press)
-
-        # Создаем вертикальный контейнер для размещения виджетов
-        vbox = BoxLayout(orientation='vertical')
-
-        # Добавляем виджеты в контейнер
-        vbox.add_widget(self.label)
-        vbox.add_widget(self.text_input)
-        vbox.add_widget(self.button)
-
-        return vbox
-
-    def on_button_press(self, instance):
-        # Получаем текст из поля ввода
-        text = self.text_input.text
-        self.label.text = text
-
-if __name__ == '__main__':
-    MyApp().run()
+HelloWorld().run()
